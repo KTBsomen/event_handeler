e68d59153a7acdcd3936ee2233e006829d789752
KTBsomen <KTBsomen@gmail.com>
2025-03-08 16:37:17 +0530

    updates

--- event_handeler/lib/event_handeler.dart
+++ event_handeler/lib/event_handeler.dart
@@ -2,17 +2,23 @@ library event_handeler;
 
 import 'dart:async';
 
-/// A Calculator.
+/// A custom event that carries data and a type.
 class CustomEvent {
-  final String data;
-  final String type;
+  /// The data associated with the event.
+  final dynamic data;
 
+  /// The type/category of the event.
+  final dynamic type;
+
+  /// Creates a new custom event with the provided [data] and [type].
   CustomEvent(this.data, this.type);
 }
 
+/// Manages event broadcasting and listening.
 class EventManager {
   static EventManager? _instance;
 
+  /// Returns a singleton instance of [EventManager].
   factory EventManager() {
     _instance ??= EventManager._internal();
     return _instance!;
@@ -20,28 +26,34 @@ class EventManager {
 
   EventManager._internal();
 
+  /// A stream controller for broadcasting events.
   final StreamController<CustomEvent> _eventController =
       StreamController<CustomEvent>.broadcast();
 
+  /// Provides a stream of incoming events.
   Stream<CustomEvent> get eventStream => _eventController.stream;
 
+  /// Dispatches an event to all active listeners.
   void dispatchEvent(CustomEvent event) {
     _eventController.sink.add(event);
   }
 
-  // void dispose() {
-  //   _eventController.close();
-  // }
+  /// Closes the event stream controller.
+  void dispose() {
+    _eventController.close();
+  }
 }
 
-dispatchCustomEvent(var eventValue, var type) {
+/// Dispatches a new event with the given [eventValue] and [type].
+void dispatchCustomEvent(var eventValue, var type) {
   final event = CustomEvent(eventValue, type);
   EventManager().dispatchEvent(event);
 }
 
-addCustomEventListener(listenForType, callback) {
-  EventManager().eventStream.listen((event) {
-    print("ok listing..${event.data} ${event.type}");
+/// Listens for events of a specific [listenForType] and executes the [callback] when an event is received.
+StreamSubscription addCustomEventListener(
+    String listenForType, Function(dynamic) callback) {
+  return EventManager().eventStream.listen((event) {
     if (event.type == listenForType) {
       callback(event.data);
     }
